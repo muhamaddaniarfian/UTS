cad432cd7422ea1d6c202906471c77ed773dafbb
oded05 <50137678+oded05@users.noreply.github.com>
2020-04-23 14:07:59 +0700

    Update uts.php

--- uts.php
+++ uts.php
@@ -13,4 +13,25 @@
 	fwrite($file, "Data Pemantauan Covid19 wilayah " . $wilayah . "\nper " . date("d-F-Y") . " " . date("H:i:s") . "\n" . $nama . "/" . $nim . "\n\nPositif | Dirawat | Sembuh | Meninggal\n" . $positif . " | " . $rawat . " | " . $sembuh . " | " . $meninggal);  
 	
 	fclose($file);
+	echo "Data Pemantauan Covid19 wilayah " . $wilayah ;
+	echo "<br>";
+	echo "per " . date("d-F-Y") . " " . date("H:i:s");
+	echo "<br>";
+	echo  $nama . "/" . $nim ;
+	echo "<br>";
+	echo "<br>";
+	echo "<table width = 500 border =1>
+				<tr>
+				<td>Positif</td>
+				<td>Dirawat</td>
+				<td>Sembuh</td>
+				<td>Meninggal</td>
+				</tr>
+				<tr>
+				<td>" . $positif . "</td>
+				<td>" . $rawat . "</td>
+				<td>" . $sembuh . "</td>
+				<td>" . $meninggal . "</td>
+				</tr>
+				</table>";
  ?>
